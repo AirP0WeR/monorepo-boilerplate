d7ed766c2609f1c653151c2c58e9ab483f5938f7
AirP0WeR <mgyp@mail.ru>
2024-02-08 14:56:36 +0300

    feat: user role name

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,3 +1,4 @@
+import pino from "@package/pino-logger";
 import express from "express";
 
 import { loggerWorker } from "./src/logger";
@@ -13,4 +14,8 @@ const PORT = process.env.SERVER_PORT || 9999;
 app.listen(PORT, () => {
   loggerWorker.debug(`NODE_ENV: ${process.env.NODE_ENV}`);
   loggerWorker.debug(`LOG_LEVEL: ${process.env.LOG_LEVEL}`);
+  pino.info(`NODE_ENV: ${process.env.NODE_ENV}`);
+
+  pino.info(new Error("kaboom"), "with hello");
+  pino.error(new Error("an error"));
 });
